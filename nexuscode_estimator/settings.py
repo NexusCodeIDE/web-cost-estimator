4b633cb6a4ee0e2e7bf0670f90a987c26ba89c8d
Jesus Zambrano <jesuszambrano@Jesuss-MBP.attlocal.net>
2025-05-02 12:50:14 -0500

    Fix static structure for Render deployment

--- nexuscode_estimator/settings.py
+++ nexuscode_estimator/settings.py
@@ -129,7 +129,7 @@ ALLOWED_HOSTS = ['nexuscode-estimator.onrender.com']
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'estimator/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
