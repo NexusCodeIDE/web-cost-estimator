7239d434d993365848d93e35bb21770baf58646f
Jesus Zambrano <jesuszambrano@Jesuss-MBP.attlocal.net>
2025-05-02 13:33:28 -0500

    Enable WhiteNoise for static file serving

--- nexuscode_estimator/settings.py
+++ nexuscode_estimator/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s-106s*8_l*kd=qs!zkvhf0ac_0z9uj&aekyx#hz*amg)z=shw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'nexuscode_estimator.urls'
