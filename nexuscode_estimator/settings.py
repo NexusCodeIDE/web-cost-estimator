7847deab69558e23bc7f3721a0b20a11154f2806
Jesus Zambrano <jesuszambrano@Jesuss-MBP.attlocal.net>
2025-05-02 12:41:57 -0500

    Fix static path and duplicate ALLOWED_HOSTS issue

--- nexuscode_estimator/settings.py
+++ nexuscode_estimator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s-106s*8_l*kd=qs!zkvhf0ac_0z9uj&aekyx#hz*amg)z=shw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
